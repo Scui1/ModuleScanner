35fb66bf31dc321fea15c487a3b9a18d638c804b
Scui1 <kaka128@gmx.de>
2022-06-16 14:33:45 +0200

    plugin for building fat jars

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     application
     kotlin("jvm") version "1.7.0"
     kotlin("plugin.serialization") version "1.7.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "de.modulescanner"
