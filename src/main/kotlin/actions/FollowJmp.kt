8608587590e15f3da5ff2e2baba573afe2bbc652
Scui <kaka128@gmx.de>
2023-03-27 15:48:48 +0200

    small refactoring for FollowJmp action and add support for E9 opcode

--- src/main/kotlin/actions/FollowJmp.kt
+++ src/main/kotlin/actions/FollowJmp.kt
@@ -2,30 +2,32 @@ package actions
 
 import pefile.PEFile
 
+const val JMP_OPCODE_SIZE = 1
+
 object FollowJmp : ExecutableAction {
     override val name = "FollowJmp"
+
+    // leaving other jump opcodes out on purpose because they are not needed
+    private val jmpInstructions = listOf(
+        JumpInstruction(0xEB.toUByte(), 1),
+        JumpInstruction(0xE8.toUByte(), 4),
+        JumpInstruction(0xE9.toUByte(), 4)
+    )
 
     override fun execute(peFile: PEFile, currentOffset: Int, arguments: List<String>): Int {
         val currentInstruction = peFile.bytes[currentOffset].toUByte()
 
-        val isShortJmp = when(currentInstruction) {
-            0xEB.toUByte() -> true
-            0xE8.toUByte() -> false
-            else -> throw ActionException("Instruction 0x${currentInstruction.toString(16)} is not a jmp.")
-        }
+        val jumpInstruction = jmpInstructions.find { it.opCode == currentInstruction }
+            ?: throw ActionException("Instruction 0x${currentInstruction.toString(16)} is not a jmp.")
 
-        // relative address is only 1 byte if it's a short jmp
-        val relAddress = if (isShortJmp)
-            peFile.bytes[currentOffset + 1].toInt()
-        else
-            peFile.readInt(currentOffset + 1)
+        val relAddress = peFile.readIntWithSize(currentOffset + JMP_OPCODE_SIZE, jumpInstruction.operandSize)
 
-        val instructionSize = if (isShortJmp) 2 else 5
-
-        val result = currentOffset + relAddress + instructionSize
+        val result = currentOffset + relAddress + JMP_OPCODE_SIZE + jumpInstruction.operandSize
         return when {
             result < peFile.bytes.size && result >= 0 -> result
             else -> throw ActionException("Jmp redirects outside module bounds. Perhaps it isn't a jmp instruction?")
         }
     }
 }
+
+data class JumpInstruction(val opCode: UByte, val operandSize: Int) {}
