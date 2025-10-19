869eb09bd6a65d6651f85836fce799c900cf00d0
Scui1 <kaka128@gmx.de>
2025-10-19 21:26:58 +0200

    add optional description for patterns

--- src/main/kotlin/json/scanrequest/Pattern.kt
+++ src/main/kotlin/json/scanrequest/Pattern.kt
@@ -3,4 +3,4 @@ package json.scanrequest
 import json.PatternType
 
 @kotlinx.serialization.Serializable
-data class Pattern(val name: String, val type: PatternType, val actions: List<Action>)
+data class Pattern(val name: String, val type: PatternType, val actions: List<Action>, val description: String? = null)
